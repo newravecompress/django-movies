d92e3223bed95868ad2d681efbf8a69b46632efb
boikov <boikov.v.y@gmail.com>
2021-08-19 14:35:56 +0300

    Split template files

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,19 @@
 from __future__ import annotations
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from django.http import HttpRequest
 
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request: HttpRequest):
-        movies = Movie.objects.all()
-        return render(request, 'movie/movie_list.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Детальная фильма"""
+    model = Movie
+    slug_field = 'url'
