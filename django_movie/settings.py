6c85757d12aff78dd49a43432ff1301922bdb159
boikov <boikov.v.y@gmail.com>
2021-08-19 11:23:00 +0300

    Add movies list template and static files

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'movie.apps.MovieConfig'
 ]
 
@@ -41,7 +42,7 @@ ROOT_URLCONF = 'django_movie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,7 +76,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -89,6 +90,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,13 @@
+from __future__ import annotations
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpRequest
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
+    def get(self, request: HttpRequest):
+        movies = Movie.objects.all()
+        return render(request, 'movie/movie_list.html', {'movie_list': movies})
