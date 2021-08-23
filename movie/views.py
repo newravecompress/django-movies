342df197e535142f392acb5a6e043aa69ebde774
boikov <boikov.v.y@gmail.com>
2021-08-23 13:32:28 +0300

    Add reviews

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from django.http import HttpRequest
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -17,3 +18,18 @@ class MovieDetailView(DetailView):
     """Детальная фильма"""
     model = Movie
     slug_field = 'url'
+
+
+class AddReview(View):
+    """Отправка отзыва"""
+
+    def post(self, request: HttpRequest, pk: int):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(pk=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get('parent', None):
+                form.parent_id = int(request.POST.get('parent'))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
